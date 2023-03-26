9267a4b0ab82c7383bd0dd8906677cba556b247b
Tyler Liu <tyler4long@gmail.com>
2023-03-25 20:04:27 -0700

    Minimal code

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,9 @@
-import JSONWorker from 'url:monaco-editor/esm/vs/language/json/json.worker.js';
-import CSSWorker from 'url:monaco-editor/esm/vs/language/css/css.worker.js';
-import HTMLWorker from 'url:monaco-editor/esm/vs/language/html/html.worker.js';
 import TSWorker from 'url:monaco-editor/esm/vs/language/typescript/ts.worker.js';
-import EditorWorker from 'url:monaco-editor/esm/vs/editor/editor.worker.js';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js';
 
 self.MonacoEnvironment = {
-  getWorkerUrl: function (moduleId, label) {
-    if (label === 'json') {
-      return JSONWorker;
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return CSSWorker;
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return HTMLWorker;
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return TSWorker;
-    }
-    return EditorWorker;
+  getWorkerUrl: function () {
+    return TSWorker;
   },
 };
 
